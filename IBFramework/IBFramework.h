22bcb3d8d42c441e53e596ad5ba0e38034128834
Nazifa Najish <nazifanajish@admins-mac-mini-3.local>
2019-01-14 17:34:11 +0530

    Initial Commit

---
+++ IBFramework/IBFramework.h
@@ -0,0 +1,19 @@
+//
+//  IBFramework.h
+//  IBFramework
+//
+//  Created by Nazifa Najish on 14/01/19.
+//  Copyright © 2019 Nazifa Najish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBFramework.
+FOUNDATION_EXPORT double IBFrameworkVersionNumber;
+
+//! Project version string for IBFramework.
+FOUNDATION_EXPORT const unsigned char IBFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBFramework/PublicHeader.h>
+
+
